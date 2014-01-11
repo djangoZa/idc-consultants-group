9eed5edc6cdc3f83b541e9ec27e71a3c406507a0
Mike <mikejohnclarke@gmail.com>
2014-01-11 17:13:10 +0200

    zurp

--- webroot/lib/bootstrap.php
+++ webroot/lib/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/classes');
+
 require_once(dirname(__FILE__) . '/classes/FloorPlan.php');
 require_once(dirname(__FILE__) . '/classes/Marker.php');
 require_once(dirname(__FILE__) . '/classes/Answer.php');
